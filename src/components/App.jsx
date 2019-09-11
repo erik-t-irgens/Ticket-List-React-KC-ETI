a53e2f160c38ef6a7be7d5b647cf95d6a514d677
Student <example@example.com>
2019-09-11 13:29:47 -0700

    up to date

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
 import NewTicketControl from './NewTicketControl';
-
+import Moment from 'moment';
 import { Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
@@ -17,9 +17,30 @@ class App extends React.Component {
 
     handleAddingNewTicketToList(newTicket) {
         var newMasterTicketList = this.state.masterTicketList.slice();
+        newTicket.formattedWaitTime = (newTicket.timeOpen).fromNow(true)
         newMasterTicketList.push(newTicket);
         this.setState({ masterTicketList: newMasterTicketList });
     }
+    componentDidMount() {
+        this.waitTimeUpdateTimer = setInterval(() =>
+            this.updateTicketElapsedWaitTime(),
+            60000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.waitTimeUpdateTimer);
+    }
+
+    updateTicketElapsedWaitTime() {
+        console.log("check");
+        let newMasterTicketList = this.state.masterTicketList.slice();
+        newMasterTicketList.forEach((ticket) =>
+            ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+        );
+        this.setState({ masterTicketList: newMasterTicketList })
+    }
+
 
     render() {
         return (
